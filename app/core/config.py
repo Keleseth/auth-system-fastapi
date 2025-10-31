bdaa8756d6ad90a515c3fdf7bb933bf4958c8f9a
Keleseth <shasa.keles@gmail.com>
2025-10-31 16:09:44 +0400

    script which create three first users with each role added to fastapi lifespan

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,18 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
 
     # UUID пользователей для проверки прав доступа
-    AUTHOR_USER_UUID: str
-    ADMIN_USER_UUID: str
+    AUTHOR_USER_UUID: str = 'fc32b64f-2599-4dd3-ac28-3a1b3fad4e30'
+    AUTHOR_EMAIL: str = 'author@yandex.ru'
+    ADMIN_USER_UUID: str = 'c50eb804-131d-4cde-a504-cea137c00dc6'
+    ADMIN_EMAIL: str = 'admin@yandex.ru'
     NOT_AUTHOR_USER_UUID: str
-    MODERATOR_USER_UUID: str
+    MODERATOR_USER_UUID: str = '4a2b3ad3-e3ca-4f71-89cf-f8907849647b'
+    MODERATOR_EMAIL: str = 'moderator@yandex.ru'
+    PASSWORD: str = 'password'
+
+    ADMIN_ROLE_NAME: str = 'admin'
+    AUTHOR_ROLE_NAME: str = 'author'
+    MODERATOR_ROLE_NAME: str = 'moderator'
 
     DB_TYPE: str
     DB_API: str
